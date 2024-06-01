02cfb9d7b0a77f0a4818446263fb2d18c78c7e09
pipiyus <156553508+pipiyus@users.noreply.github.com>
2024-05-31 20:18:26 -0400

    changes made to the project
    Changes made to the project

--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
-#This is a good programming advice
+# This is a good programming advice
 print("If the implementation is hard to explain , it's a bad idea.")
+print("If the implemention is easy to explain, it may be a good idea.")
+print ("Namespace are one honking great idea -- let's do more of those!")
